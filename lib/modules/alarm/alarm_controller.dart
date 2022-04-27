808ea31d9383fa57c97c173a60443afb4c92eea2
william-santos-dantas <nidrahdev@protonmail.com>
2022-04-27 20:07:31 -0300

    get info from android and show on screen

--- lib/modules/alarm/alarm_controller.dart
+++ lib/modules/alarm/alarm_controller.dart
@@ -1,5 +1,34 @@
+import 'package:battery_info/battery_info_plugin.dart';
+import 'package:battery_info/model/android_battery_info.dart';
 import 'package:get/get.dart';
 
 class AlarmController extends GetxController {
-  
+  final _androidBatteryInfo = AndroidBatteryInfo().obs;
+  get androidBatteryInfo => _androidBatteryInfo.value;
+  final _estimatedTime = ''.obs;
+  get estimatedTime => _estimatedTime.value;
+
+  @override
+  void onReady() {
+    super.onReady();
+    getAndroidInfo();
+  }
+
+  void getAndroidInfo() {
+    BatteryInfoPlugin().androidBatteryInfoStream.listen(
+      (AndroidBatteryInfo? batteryInfo) {
+        if (batteryInfo != null) {
+          _androidBatteryInfo(batteryInfo);
+          double remainingEnergy = -(batteryInfo.remainingEnergy! * 1.0E-9);
+          int estimateHour =
+              ((batteryInfo.batteryCapacity! / 1000) / remainingEnergy) ~/ 60;
+          double estimateMinutes =
+              ((batteryInfo.batteryCapacity! / 1000) / remainingEnergy) % 60;
+          _estimatedTime(
+            "${estimateHour}h${estimateMinutes.round() < 10 ? '0${estimateMinutes.round()}m' : '${estimateMinutes.round()}m'}",
+          );
+        }
+      },
+    );
+  }
 }
--- lib/modules/alarm/alarm_page.dart
+++ lib/modules/alarm/alarm_page.dart
@@ -1,3 +1,4 @@
+import 'package:battery_info/enums/charging_status.dart';
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 
@@ -15,77 +16,103 @@ class AlarmPage extends GetView<AlarmController> {
           double size = orientation == Orientation.portrait
               ? context.widthTransformer(reducedBy: 20)
               : context.heightTransformer(reducedBy: 40);
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 20),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        SizedBox(
-                          width: size,
-                          height: size,
-                          child: const CircularProgressIndicator(
-                            value: 100,
-                            semanticsLabel: "Circular progress indicator",
-                            backgroundColor: Colors.grey,
-                            strokeWidth: 20,
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              Colors.blue,
+          return Obx(
+            () => SingleChildScrollView(
+              child: Column(
+                children: [
+                  SafeArea(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox(
+                            width: size,
+                            height: size,
+                            child: CircularProgressIndicator(
+                              value: controller
+                                          .androidBatteryInfo.chargingStatus ==
+                                      ChargingStatus.Charging
+                                  ? null
+                                  : (controller.androidBatteryInfo
+                                              .batteryLevel ??
+                                          0) /
+                                      100,
+                              semanticsLabel: "Circular progress indicator",
+                              backgroundColor: Colors.grey,
+                              strokeWidth: 20,
+                              valueColor:
+                                  (controller.androidBatteryInfo.batteryLevel ??
+                                              0) <
+                                          15
+                                      ? const AlwaysStoppedAnimation<Color>(
+                                          Colors.red)
+                                      : const AlwaysStoppedAnimation<Color>(
+                                          Colors.blue,
+                                        ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          width: size,
-                          height: size,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Text(
-                                "100%",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.bold,
+                          SizedBox(
+                            width: size,
+                            height: size,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${controller.androidBatteryInfo.batteryLevel}%",
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              SizedBox(height: 10),
-                              RotatedBox(
-                                quarterTurns: 3,
-                                child: Icon(
-                                  Icons.battery_full,
-                                  color: Colors.white,
-                                  size: 70,
+                                const SizedBox(height: 10),
+                                RotatedBox(
+                                  quarterTurns: 3,
+                                  child: Icon(
+                                    controller.androidBatteryInfo
+                                                .chargingStatus ==
+                                            ChargingStatus.Charging
+                                        ? Icons.battery_charging_full
+                                        : Icons.battery_full,
+                                    color: Colors.white,
+                                    size: 70,
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    MobileInfoRow(
-                      text: "GOOD ",
-                      icon: Icons.favorite,
-                    ),
-                    MobileInfoRow(
-                      text: "12:00",
-                      icon: Icons.schedule,
-                    ),
-                    MobileInfoRow(
-                      text: "30°C",
-                      icon: Icons.device_thermostat,
-                      iconColor:Colors.white,
-                    ),
-                  ],
-                ),
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      MobileInfoRow(
+                        text:
+                            controller.androidBatteryInfo.health == "health_good"
+                                ? "GOOD"
+                                : "BAD",
+                        icon: Icons.favorite,
+                      ),
+                      MobileInfoRow(
+                        text: "${controller.estimatedTime}",
+                        icon: Icons.schedule,
+                      ),
+                      MobileInfoRow(
+                        text: "${controller.androidBatteryInfo.temperature}°C",
+                        icon: Icons.device_thermostat,
+                        iconColor:
+                            (controller.androidBatteryInfo.temperature ?? 0) >
+                                    35
+                                ? Colors.red
+                                : Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         },
